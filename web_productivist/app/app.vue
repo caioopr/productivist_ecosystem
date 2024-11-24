b6b03d9ef0f9fa675e1265704b26471c5f2ad7ec
Caio Prado <caioppubli@gmail.com>
2024-11-24 00:50:12 -0300

    V4 Compat. & NuxtUI Module

---
+++ web_productivist/app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const name = "aaaaa";
+
+function printname(name:string){
+  console.log(name)
+}
+</script>
