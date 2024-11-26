42b3510479e0cfd265ba2f680f5615a442953650
Caio Prado <caioppubli@gmail.com>
2024-11-26 10:56:52 -0300

    Project Selection menu

--- web_productivist/app/pages/tasks.vue
+++ web_productivist/app/pages/tasks.vue
@@ -6,18 +6,22 @@ const selectedProject = ref(projectsOptions[0])
 
 <template>
   <section class="flex items-center justify-between mb-10">
-    <!-- <div class="flex items-center gap-1">
+    <div class="flex items-center gap-1">
         <p>Project:</p>
+
         <UButtonGroup size="sm" orientation="horizontal">
-          <USelectMenu :options="projectsOptions" v-model="selectedProject" class="ring-transparent outline-transparent"/>
+          <USelectMenu :options="projectsOptions" v-model="selectedProject" :ui="{color: {white: { outline: 'shadow-sm bg-white dark:bg-gray-900 text-gray-900 dark:text-white ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:ring-2 focus:ring-black dark:focus:ring-white' }}}"/>
           <UButton icon="i-heroicons-plus" color="white"/>
         </UButtonGroup>
-    </div> -->
+
+        <div class="flex items-center gap-1">
+          <UButton label="Constants" color="white" variant="outline"/>
+          <UButton label="Filter" color="white" variant="outline"/>
+        </div>
+    </div>
     
-    <div class="flex items-center gap-1">
-        <UButton label="Constants" color="white" variant="outline"/>
-        <UButton label="Filter" color="white" variant="outline"/>
-      </div>
+    
       <UButton label="New" color="black" icon="i-heroicons-plus"/>
   </section>
 </template>
+
